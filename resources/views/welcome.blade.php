b7e5784b63dac4c7c4bad0e9292912bac113f09a
saad <qwejtrs@gmail.com>
2025-03-27 03:55:17 +0200

    all done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@ App::setLocale(session('locale', 'ar'));
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>صفحة مشابهة</title>
+    <title>UNHCR</title>
     <style>
         body {
             font-family: Arial, sans-serif;
